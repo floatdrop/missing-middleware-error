0cb8ba870e95e228aae9d24bc5a9001b5971efee
Vsevolod Strukchinsky <floatdrop@gmail.com>
2015-08-11 20:45:15 +0500

    Add name argument validation

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ var createErrorClass = require('create-error-class');
 
 /* eslint-disable no-invalid-this */
 module.exports = createErrorClass('MissingMiddleware', function (name) {
+	if (!name) {
+		throw new Error('name argument is required');
+	}
+
 	this.message = name + '\n';
 	this.message += '\tAdd it before using this middleware: app.use(require(\'' + name + '\')());\n';
 	this.message += '\tOr read documentation at http://npmjs.com/' + name;
